9267c45f20d7a7264b688ba7713c84238252b492
C12H22O12 <director20844@gmail.com>
2021-06-15 16:59:42 +0900

    21/06/15 "즐겨찾기 추가 및 취소, 엘리먼트 삭제"

--- script/add.js
+++ script/add.js
@@ -1,8 +1,12 @@
 function getEle() {
     let cardBody = document.getElementById("cardBody");
 
-    let title = document.getElementById("title").value;
-    let about = document.getElementById("about").value;
+    let title = document
+        .getElementById("title")
+        .value;
+    let about = document
+        .getElementById("about")
+        .value;
 
     let newDiv = document.createElement('div');
     let newH1 = document.createElement('h1');
@@ -10,11 +14,21 @@ function getEle() {
     let newWho = document.createElement('div');
     let img = document.createElement('img');
     let starImg = document.createElement('img');
+    let cancelImg = document.createElement('img');
 
-    img.setAttribute("src","asset/typewriter-801921_1920.jpg");
-    
-    starImg.setAttribute("src", "asset/star_blanks.png");
-    starImg.classList.add("star");
+    img.setAttribute("src", "asset/typewriter-801921_1920.jpg");
+
+    starImg.setAttribute("src", "asset/star_blank.png");
+    starImg
+        .classList
+        .add("star");
+    starImg.onclick = addStar;
+
+    cancelImg.setAttribute("src", "asset/cancel.png");
+    cancelImg
+        .classList
+        .add("cancel");
+    cancelImg.onclick = cancel;
 
     let addTitle = document.createTextNode(title);
     newH1.appendChild(addTitle);
@@ -24,10 +38,15 @@ function getEle() {
 
     let addWho = document.createTextNode('made BY Who');
     newWho.appendChild(addWho);
-    newWho.classList.add("from");
+    newWho
+        .classList
+        .add("from");
 
-    newDiv.classList.add("card");
+    newDiv
+        .classList
+        .add("card");
     newDiv.append(starImg);
+    newDiv.append(cancelImg);
     newDiv.append(img);
     newDiv.append(newH1);
     newDiv.append(newP);
@@ -36,5 +55,17 @@ function getEle() {
 }
 
 function cancel() {
+    let target = event.currentTarget.parentNode;
+    target.remove();
+}
+
+function addStar() {
+    let target = event.currentTarget;
     
+    //img src로 판단하는 것인데 기준을 좀 더 생각해봐야할 듯 
+    if (target.src == "http://127.0.0.1:5500/asset/star.png") {
+        target.src = "asset/star_balnk.png";
+    } else {
+        target.src = "asset/star.png";
+    }
 }
